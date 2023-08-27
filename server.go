6898f02ca2bc0b809f8f5241f03a248a83d83f36
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-08-27 15:58:28 +0700

    Update server.go

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "OK!")
-    fmt.Println("Endpoint Hit: homePage")
+    fmt.Println("Endpoint Hit: homePage1")
 }
 
 func handleRequests() {
