1b2310593a7e9a4e8392ae6d0c3da0e9ece89639
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-09-15 08:18:34 +0700

    Update auth.go

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"fmt"
 	"goobike-backend/common"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,12 +11,21 @@ import (
 func Auth() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		tokenString := context.GetHeader("Authorization")
-		if tokenString == "" {
+		words := strings.Fields(tokenString)
+		fmt.Println(words[0])
+
+		if words[0] != "bearer" {
+			context.JSON(401, gin.H{"error": "request does not contain an access token"})
+			// context.Abort()
+			return
+		}
+
+		if words[1] == "" {
 			context.JSON(401, gin.H{"error": "request does not contain an access token"})
 			context.Abort()
 			return
 		}
-		err := common.ValidateToken(tokenString)
+		err := common.ValidateToken(words[1])
 		if err != nil {
 			context.JSON(401, gin.H{"error": err.Error()})
 			context.Abort()
